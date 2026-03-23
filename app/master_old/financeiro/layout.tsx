12d303ffd01d3831cd6db06866a2e4466105a969
Renato Silva <realimperiodigital@gmail.com>
2026-03-23 08:01:05 -0300

    fix: reorganiza financeiro dentro do dashboard

--- app/master_old/financeiro/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-type Props = {
-  children: ReactNode;
-};
-
-export default function MasterFinanceiroLayout({ children }: Props) {
-  return <>{children}</>;
-}
--- app/master_old/layout.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function MasterLayout({ children }: Props) {
-  return <>{children}</>;
-}
